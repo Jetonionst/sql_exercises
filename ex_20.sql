3eb778b98ae5b560e7cfe7d3be258a4d21d2eae3
Jetonionst <52862613+Jetonionst@users.noreply.github.com>
2022-05-16 14:47:11 +0600

    Create ex_20.sql

---
+++ ex_20.sql
@@ -0,0 +1,6 @@
+SELECT sum(I.Total) as TotalSum, (E.FirstName || ' ' || E.LastName) as Name FROM Customer C
+JOIN Employee E on E.EmployeeId = C.SupportRepId
+JOIN Invoice I on C.CustomerId = I.CustomerId
+WHERE title like '%sales%' or '%agent%'
+GROUP BY Name
+ORDER BY TotalSum DESC;
