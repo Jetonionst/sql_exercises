43aed9f7fa1aaed4d03752b739f9eca6898cf576
Jetonionst <52862613+Jetonionst@users.noreply.github.com>
2022-05-16 15:45:31 +0600

    Create ex_24.sql

---
+++ ex_24.sql
@@ -0,0 +1,5 @@
+SELECT T.Name AS TrackName, count(I.InvoiceId) AS Count FROM InvoiceLine
+JOIN Track T on InvoiceLine.TrackId = T.TrackId
+JOIN Invoice I on I.InvoiceId = InvoiceLine.InvoiceId
+GROUP BY T.TrackId
+ORDER BY Count DESC;
