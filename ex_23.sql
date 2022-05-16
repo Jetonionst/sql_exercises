66e4f23c220c5f71fc8c0429428b2c51c8195bd4
Jetonionst <52862613+Jetonionst@users.noreply.github.com>
2022-05-16 15:16:22 +0600

    Create ex_23.sql

---
+++ ex_23.sql
@@ -0,0 +1,6 @@
+SELECT T.Name AS TrackName, count(I.InvoiceId) AS Count FROM InvoiceLine
+JOIN Track T on InvoiceLine.TrackId = T.TrackId
+JOIN Invoice I on I.InvoiceId = InvoiceLine.InvoiceId
+WHERE strftime('%Y',InvoiceDate)= '2013'
+GROUP BY TrackName
+ORDER BY Count DESC;
