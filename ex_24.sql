a104485787a6bb42b8d926dd2e1a22741922d9e8
Jetonionst <52862613+Jetonionst@users.noreply.github.com>
2022-05-23 17:52:35 +0600

    Update ex_24.sql

--- ex_24.sql
+++ ex_24.sql
@@ -2,4 +2,5 @@ SELECT T.Name AS TrackName, count(I.InvoiceId) AS Count FROM InvoiceLine
 JOIN Track T on InvoiceLine.TrackId = T.TrackId
 JOIN Invoice I on I.InvoiceId = InvoiceLine.InvoiceId
 GROUP BY T.TrackId
-ORDER BY Count DESC;
+ORDER BY Count DESC
+LIMIT 5;
