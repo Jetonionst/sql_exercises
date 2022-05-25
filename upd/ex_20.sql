d18eb71c30741453eafcbf709ebdafe35ab3b159
Jetonionst <52862613+Jetonionst@users.noreply.github.com>
2022-05-25 12:29:47 +0600

    Update ex_20.sql

--- upd/ex_20.sql
+++ upd/ex_20.sql
@@ -3,4 +3,4 @@ SELECT sum(I.Total) as TotalSum, (E.FirstName || ' ' || E.LastName) as Name FROM
 JOIN Employee E on E.EmployeeId = C.SupportRepId
 JOIN Invoice I on C.CustomerId = I.CustomerId
 WHERE title like '%sales%' or '%agent%'
-GROUP BY Name)
+GROUP BY c.SupportRepId)
