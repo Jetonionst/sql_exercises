3f72ff0b23323809d14c64a43febf3e2895e37d6
Jetonionst <52862613+Jetonionst@users.noreply.github.com>
2022-05-25 12:27:59 +0600

    Update ex_17.sql

--- upd/ex_17.sql
+++ upd/ex_17.sql
@@ -1,4 +1,4 @@
 SELECT sum(I.Total) as TotalSum, (E.FirstName || ' ' || E.LastName) as Name FROM Customer C
 JOIN Employee E on E.EmployeeId = C.SupportRepId
 JOIN Invoice I on C.CustomerId = I.CustomerId
-GROUP BY Name;
+GROUP BY c.SupportRepId;
